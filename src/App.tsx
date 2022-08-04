000a1f85df3b3e3567a51ec70c1e75694b8910fb
Jason Pacheco <pacheco.jason93@gmail.com>
2022-08-04 16:03:40 -0700

    chore: update template

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
-export const App = () => {
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const App = () => {
   return <div>Vite React Start App</div>;
+};
+
+export const mountApp = () => {
+  const container = document.getElementById('root');
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const root = createRoot(container!);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
 };
